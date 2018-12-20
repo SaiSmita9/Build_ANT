679f23cb8094a257b9c8f5cca23efd1bd12ff657
jyothsna553 <36263180+jyothsna553@users.noreply.github.com>
2018-12-20 16:20:00 +0530

    Update AntClass.java

--- src/testANT/AntClass.java
+++ src/testANT/AntClass.java
@@ -6,7 +6,8 @@ public class AntClass {
 
 	public static void main(String[] args){									       
 		System.out.println("HELLO GURU99 ANT PROGRAM");
-		system.out.println("Hai Smita.how are you");
+		System.out.println("Hai Smita.how are you");
+		System.out.println("Hai Jyothsna.how are you");
 		System.out.println("TODAY's DATE IS->"+ currentDate() );					  
 }		    		   
 public static String currentDate(){					        
